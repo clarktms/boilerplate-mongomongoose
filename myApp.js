b5da4f691822344d516a7171021535c6213a90ff
clarktms <clarktms@gmail.com>
2023-05-17 20:58:40 +0000

    throughFindEditThenSave

--- myApp.js
+++ myApp.js
@@ -56,17 +56,29 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, function(err, data) {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById({ _id: personId }, function(err, data) {
+    if (err) return console.error(err);
+    done(null, data);
+  })
 };
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+  Person.findById({ _id: personId }, function(err, data) {
+    if (err) return console.error(err);
+    data.favoriteFoods.push(foodToAdd);
+    data.save((err, updatedPerson) => {
+      done(null, updatedPerson)
+    })
+  })
 };
 
 const findAndUpdate = (personName, done) => {
